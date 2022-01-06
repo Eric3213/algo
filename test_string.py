8918e9708c722bd510bcddc32b994990307876b1
eric <1832871204@qq.com>
2022-01-06 21:18:58 +0800

    guyongzhe

--- test_string.py
+++ test_string.py
@@ -29,10 +29,136 @@ def modifyString(s: str) -> str:
     for i in range(n):
         if temp[i] == '?':
             for change in "abc":
-                if not (i > 0 and temp[i-1] == change or i < n - 1 and temp[i+1] == change):
+                if not (i > 0 and temp[i - 1] == change or i < n - 1 and temp[i + 1] == change):
                     temp[i] = change
                     break
     return "".join(temp)
+
+
+def calculate(s: str):
+    ops = [1]
+    sign = 1
+    n = len(s)
+    ans = 0
+    i = 0
+    while i < n:
+        if s[i] == ' ':
+            i += 1
+        elif s[i] == '+':
+            sign = ops[-1]
+            i += 1
+        elif s[i] == '-':
+            sign = -ops[-1]
+            i += 1
+        elif s[i] == '(':
+            ops.append(sign)
+            i += 1
+        elif s[i] == ')':
+            ops.pop()
+            i += 1
+        else:
+            num = 0
+            while i < n and s[i].isdigit():
+                num = num * 10 + ord(s[i]) - ord('0')
+                i += 1
+            ans += num * sign
+    return ans
+
+
+def numDecoding(s: str):
+    n = len(s)
+    # f = [1] + [0] * n
+    # for i in range(1, n+1):
+    #     if s[i-1] != '0':
+    #         f[i] += f[i-1]
+    #     if i > 1 and s[i-2] != '0' and int(s[i-2:i]) <= 26:
+    #         f[i] += f[i-2]
+    # return f[n]
+    a, b, c = 0, 1, 0
+    for i in range(1, n + 1):
+        c = 0
+        if s[i - 1] != '0':
+            c += b
+        if i > 1 and s[i - 2] != '0' and int(s[i - 2:i]) <= 26:
+            c += a
+        a, b = b, c
+    return c
+
+
+def simplifyPath(path: str):
+    names = path.split("/")
+    stack = list()
+    for name in names:
+        if name == "..":
+            if stack:
+                stack.pop()
+        elif name and name != ".":
+            stack.append(name)
+    return "/" + "/".join(stack)
+
+
+def longestValidParentheses(s: str):
+    """
+    方法一：动态规划
+    方法二：栈
+    方法三：降低空间复杂度
+    :param s:
+    :return:
+    """
+    # n = len(s)
+    # dp = [0] * n
+    # if n < 2:
+    #     return 0
+    # for i in range(1, n):
+    #     if s[i] == ')':
+    #         if s[i-1] == '(':
+    #             dp[i] = dp[i-2] + 2 if i > 1 else 2
+    #         else:
+    #             if i - dp[i-1] > 0 and s[i - dp[i-1] - 1] == '(':
+    #                 dp[i] = dp[i-1] + 2 if i - dp[i-1] - 2 < 0 else dp[i-1] + dp[i - dp[i-1] - 2] + 2
+    # return max(dp)
+
+    # 方法二
+    # n = len(s)
+    # stack = [-1]
+    # ans = 0
+    # for i in range(n):
+    #     if s[i] == "(":
+    #         stack.append(i)
+    #     else:
+    #         stack.pop()
+    #         if not stack:
+    #             stack.append(i)
+    #         else:
+    #             tempLength = i - stack[-1]
+    #             ans = max(tempLength, ans)
+    # return ans
+
+    # 方法三 贪
+    n = len(s)
+    left, right = 0, 0
+    maxLength = 0
+    for i in range(n):
+        if s[i] == "(":
+            left += 1
+        else:
+            right += 1
+        if right == left:
+            maxLength = max(maxLength, right * 2)
+        elif right > left:
+            left, right = 0, 0
+    left, right = 0, 0
+    for i in range(n-1, -1, -1):
+        if s[i] == "(":
+            left += 1
+        else:
+            right += 1
+        if right == left:
+            maxLength = max(maxLength, right * 2)
+        elif left > right:
+            left, right = 0, 0
+    return maxLength
+
 
 
 if __name__ == "__main__":
--- test_tree.py
+++ test_tree.py
@@ -150,6 +150,33 @@ class Solution:
                 root.left = self.deleteNode(root.left, root.val)
         return root
 
+    def isValidBST(self, root: TreeNode):
+        # def helper(node, lower=float('-inf'), upper=float('inf')):
+        #     if not node:
+        #         return True
+        #     val = node.val
+        #     if val <= lower or val >= upper:
+        #         return False
+        #     if not helper(node.left, lower, val):
+        #         return False
+        #     if not helper(node.right, val, upper):
+        #         return False
+        #     return True
+        #
+        # return helper(root)
+        stack, inorder = [], float('-inf')
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
+        return True
+
+
 
 class Codec:
 
@@ -179,7 +206,6 @@ def pre_order(root):
     ans.append(root.val)
     pre_order(root.left)
     pre_order(root.right)
-
 
 
 ser = Codec()
