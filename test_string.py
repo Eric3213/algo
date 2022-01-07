d8a223b90871898bc72e316491203a69322f6490
eric <1832871204@qq.com>
2022-01-07 18:38:11 +0800

    a

--- test_string.py
+++ test_string.py
@@ -190,6 +190,26 @@ def maxDepth(s: str):
     return ans
 
 
+def validIPAddress(queryIP: str):
+    if queryIP.count(".") == 3:
+        nums = queryIP.split(".")
+        for x in nums:
+            if len(x) == 0 or len(x) > 3:
+                return "Neither"
+            if (x[0] == '0' and len(x) != 1) or not x.isdigit() or int(x) > 255:
+                return "Neither"
+        return "IPv4"
+    elif queryIP.count(":") == 7:
+        nums = queryIP.split(":")
+        hexdigits = "0123456789abcdefABCDEF"
+        for x in nums:
+            if len(x) == 0 or len(x) > 4 or not all(c in hexdigits for c in x):
+                return "Neither"
+        return "IPv6"
+    else:
+        return "Neither"
+
+
 if __name__ == "__main__":
     # s = "owoztneoer"
     # print(originalDigits(s))
