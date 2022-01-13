1959d1578768fbb6680780ad4838606b0f17823f
eric <1832871204@qq.com>
2022-01-13 21:27:57 +0800

    a

--- test_array.py
+++ test_array.py
@@ -341,6 +341,7 @@ def permuteUnique(nums: List[int]):
     return ans
 
 
+<<<<<<< HEAD
 def minSwaps(nums: List[int]):
     n = len(nums)
     m = nums.count(1)
@@ -375,15 +376,52 @@ def minSwaps(nums: List[int]):
         ans = max(ans, cur)
     return m - ans
 
+=======
+def dominantIndex(nums: List[int]):
+    m1, m2, idx = -1, -1, 0
+    for i, n in enumerate(nums):
+        if n > m1:
+            m1, m2, idx = n, m1, i
+        elif n > m2:
+            m2 = n
+    return idx if m1 >= m2 * 2 else -1
+    # n = len(nums)
+    # if n == 1:
+    #     return 0
+    # if nums[0] >= nums[1]:
+    #     largest, secondLarge = nums[0], nums[1]
+    #     largestIndex = 0
+    # else:
+    #     largest, secondLarge = nums[1], nums[0]
+    #     largestIndex = 1
+    # for i in range(2, n):
+    #     if nums[i] > largest:
+    #         secondLarge = largest
+    #         largest = nums[i]
+    #         largestIndex = i
+    #     elif nums[i] > secondLarge:
+    #         secondLarge = nums[i]
+    # print(largest)
+    # print(secondLarge)
+    # if largest // 2 >= secondLarge:
+    #     return largestIndex
+    # else:
+    #     return -1
+>>>>>>> b6845f2c162f338ae249be4556998db17fe03237
 
 
 
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     a = [0, 1, 0, 1, 1, 0, 0]
     b = [0, 1, 1, 1, 0, 0, 1, 1, 0]
     c = [1, 1, 0, 0, 1]
     print(minSwaps(a))
+=======
+    a = [1, 2, 3, 4]
+    print(dominantIndex(a))
+>>>>>>> b6845f2c162f338ae249be4556998db17fe03237
     # a = [2,2,3]
     # print(permuteUnique(a))
     # a = "B0B6G0R6R0R6G9"
